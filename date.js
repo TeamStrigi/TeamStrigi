098acd2097ad248e9bc9a723e863306fad2c1ad0
TeamStrigi <76907050+TeamStrigi@users.noreply.github.com>
2021-01-06 04:27:40 +0700

    Update date.js

--- date.js
+++ date.js
@@ -1,5 +1,4 @@
 <div style="text-align: center;">
-<script type="text/javascript">
 now = new Date();
 if (now.getTimezoneOffset() == 0) (a=now.getTime() + (7*60*60*1000))
 else (a=now.getTime());
@@ -11,5 +10,4 @@ var tanggal=now.getDate()
 var hariarray=new Array("Minggu,","Senin,","Selasa,","Rabu,","Kamis,","Jum'at,","Sabtu,")
 var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember")
 document.write(hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun)
-</script>
 </div>
